d83645092f4701f88860591153a42486467b456d
2V01 <74354169+2V01@users.noreply.github.com>
2020-12-13 16:58:15 +0300

    Add files via upload

---
+++ Library/SaveLog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Library
+{
+    public delegate void Message(string mes);
+    public class SaveLog
+    {
+        string way;
+        public event Message mes;
+        public SaveLog()
+        {
+        }
+        public SaveLog(string way)
+        {
+            this.way = way;
+        }
+
+        public async void AddLog(string text)
+        {
+            if (way != null)
+            {
+                using (FileStream fstream = new FileStream(@$"{way}\log.txt", FileMode.OpenOrCreate))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    await fstream.WriteAsync(array, 0, array.Length);
+                    mes?.Invoke("Запись по заданному пути произошла успешно!");
+                }
+            }
+            else
+            {
+                using (FileStream fstream = new FileStream(@$"log.txt", FileMode.OpenOrCreate))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                    await fstream.WriteAsync(array, 0, array.Length);
+                    mes?.Invoke("Запись по стандартному пути!");
+                }
+            }
+
+        }
+    }
+}
